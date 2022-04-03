2a1acc394b66bae63babbae56981251430f4bbb0
Piyush1321 <piyushkumar1321@gmail.com>
2022-04-03 15:24:49 +0530

    update

---
+++ src/components/util/questions/MCQSingleCorrectTemplate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class MCQSingleCorrectTemplate extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <form>
+                MCQ SC question template
+            </form>
+        )
+    }
+}
+
+export default MCQSingleCorrectTemplate;
---
+++ src/components/util/questions/ProgrammingTemplate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ProgrammingTemplate extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <form>
+                Programming question template
+            </form>
+        )
+    }
+}
+
+export default ProgrammingTemplate;
